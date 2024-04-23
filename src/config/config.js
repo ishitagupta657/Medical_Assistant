4cfb7624e8b450f4ca02e3b70be30739762ba9ae
Ishita gupta <ishitagupta@Ishitas-MacBook-Air.local>
2024-04-23 18:21:27 +0530

    Removed password

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   env: "dev",
   port: 3000,
   mongoose: {
-    url: "mongodb+srv://medicalassitant:QR6rbWf03uzYl6I4@cluster0.up0zfdv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    url: "mongodb+srv://<username>:<password>@cluster0.up0zfdv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     options: {
       useCreateIndex: true,
       useNewUrlParser: true,
